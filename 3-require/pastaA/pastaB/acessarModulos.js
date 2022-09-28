4439a1fd20835ec5d2bc90b5a75aa6c922ece9af
Eduardo Conde Pires <eduardo.pires@threeo.com.br>
2022-09-28 15:56:46 -0300

    Sistema de módulos: require

---
+++ 3-require/pastaA/pastaB/acessarModulos.js
@@ -0,0 +1,11 @@
+const moduloA = require('../../../1-modulos/moduloA')
+console.log(moduloA.ola)
+
+const c = require('./pastaC')
+console.log(c.ola2)
+
+// const http = require('http')
+// http.createServer((req, res) => {
+//     res.write('Boa tarde!')
+//     res.end()
+// }).listen(8080)
